5a58047765f9230fe52f5533dc85caec178e4830
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-08-26 13:11:52 +0900

    .

---
+++ java01/src/net/bitacademy/java41/step15/test/ThreadTest01.java
@@ -0,0 +1,45 @@
+package net.bitacademy.java41.step15.test;
+
+/* JVM이 관리하는 스레드 */
+public class ThreadTest01 {
+
+	public static void main(String[] args) {
+		Thread t1 = Thread.currentThread();
+		System.out.println("1:" + t1.getName()); // main thread
+
+		ThreadGroup tg = t1.getThreadGroup();
+		System.out.println("2:" + tg.getName()); // main tread group
+		
+		ThreadGroup tg2 = null;
+		if (tg.getParent() != null) {
+			tg2 = tg.getParent();
+			System.out.println("3:" + tg2.getName()); // system thread group
+		}
+		
+		if (tg2.getParent() != null) { // system's parent thread group is null.
+			tg2 = tg2.getParent();
+			System.out.println("4:" + tg2.getName());
+		}
+		
+		tg2.list();
+		/*
+java.lang.ThreadGroup[name=system,maxpri=10]
+Thread[Reference Handler,10,system]
+Thread[Finalizer,8,system]
+Thread[Signal Dispatcher,9,system]
+java.lang.ThreadGroup[name=main,maxpri=10]
+    Thread[main,5,main]
+		 */
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
