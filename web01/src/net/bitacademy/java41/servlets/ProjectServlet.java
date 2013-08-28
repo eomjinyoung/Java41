65841fe45868a7eedaa1be5da1ab7428bf144669
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-08-28 16:50:06 +0900

    .

--- web01/src/net/bitacademy/java41/servlets/ProjectServlet.java
+++ web01/src/net/bitacademy/java41/servlets/ProjectServlet.java
@@ -1,66 +1,66 @@
 package net.bitacademy.java41.servlets;
 
-import java.io.PrintStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Date;
 import java.util.List;
-import java.util.Scanner;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
 import net.bitacademy.java41.dao.ProjectDao;
+import net.bitacademy.java41.util.DBConnectionPool;
 import net.bitacademy.java41.vo.Project;
 
-public class ProjectServlet {
+@WebServlet("/project")
+@SuppressWarnings("serial")
+public class ProjectServlet extends GenericServlet {
 	private Project project;
 	private ProjectDao projectDao;
 	
-	public ProjectServlet(ProjectDao projectDao) {
-		this.projectDao = projectDao;
+	public ProjectServlet() {
+		DBConnectionPool dbpool = new DBConnectionPool(
+				"jdbc:mysql://localhost/test", 
+				"test", 
+				"test",
+				System.getProperty("driverClass"));
+		this.projectDao = new ProjectDao(dbpool);
 	}
 
-	public void execute(Scanner scanner, PrintStream out) {
-		while(true) {
-			printPrompt(out);
-			
-			String command = scanner.nextLine();
-
-			if (command.equals("list")) {
-				project = null;
-				list(scanner, out);
-			} else if (command.equals("add")) {
-				project = null;
-				add(scanner, out);
-			} else if (command.startsWith("view")) {
-				String[] values = command.split(" ");
-				view(scanner, out, Integer.parseInt(values[1]) );
-			} else if (command.equals("update")) {
-				if (project != null) {
-					update(scanner, out);
-				} else {
-					out.println("먼저 프로젝트를 조회하세요!");
-				}
-			} else if (command.equals("delete")) {
-				if (project != null) {
-					detele(scanner, out);
-				} else {
-					out.println("먼저 멤버를 조회하세요!");
-				}
-			} else if (command.equals("menu")) {
-				break;
-
+	@Override
+	public void service(ServletRequest request, ServletResponse response)
+			throws ServletException, IOException {
+		String command = request.getParameter("command");
+		response.setContentType("text/plain;charset=UTF-8");
+		
+		try {
+			if ("list".equals(command)) {
+				list(request, response);
+			} else if ("add".equals(command)) {
+				add(request, response);
+			} else if ("view".equals(command)) {
+				view(request, response);
+			} else if ("delete".equals(command)) {
+				delete(request, response);
+			} else if ("update".equals(command)) {
+				update(request, response);
 			} else {
-				out.println("지원하지 않는 명령어입니다.");
-			}
+				PrintWriter out = response.getWriter();
+				out.println("해당 명령어를 지원하지 않습니다!");
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
-	private void printPrompt(PrintStream out) {
-		out.println("프로젝트관리>");
-		out.println("@@?");
-	}
-	
-	private void list(Scanner scanner, PrintStream out) {
+	private void list(ServletRequest request, ServletResponse response) {
 		try {
 			List<Project> list = projectDao.list();
 
+			PrintWriter out = response.getWriter();
 			for(Project project : list) {
 				out.print(project.getNo() + ",");
 				out.print(project.getTitle() + ",");
@@ -72,64 +72,32 @@ public class ProjectServlet {
 		} 
 	}
 
-	private void add(Scanner scanner, PrintStream out){
+	private void add(ServletRequest request, ServletResponse response)
+		throws Exception
+	{
 		Project project = new Project();
-		out.println("관리자 이메일(예: test@test.com) : \n@@?");
-		project.setManagerEmail(scanner.nextLine());
-		out.println("제목 : \n@@?");
-		project.setTitle(scanner.nextLine());
+		project.setManagerEmail(request.getParameter("managerEmail"));
+		project.setTitle(request.getParameter("title"));
+		project.setContent(
+				request.getParameter("content").replace("\\n", "\n"));
 		
-		out.println("내용(입력종료는 빈라인) : \n@@?");
-		String temp = null;
-		StringBuffer buffer = new StringBuffer();
-		while(true) {
-			temp = scanner.nextLine();
-			if (temp.equals("")) {
-				break;
-			} else {
-				out.println("@@?");
-			}
-			buffer.append(temp + "\n");
-		}
-		project.setContent(buffer.toString());
+		project.setStartDate(Date.valueOf(request.getParameter("startDate")));
+		project.setEndDate(Date.valueOf(request.getParameter("endDate")));
 		
-		while(true) {
-			out.println("시작일(예: 2013-08-21) : \n@@?");
-			try {
-				project.setStartDate(Date.valueOf(scanner.nextLine()));
-				break;
-			} catch (Exception e) {
-				out.println("날짜 형식이 맞지 않습니다. 다시 입력하세요!");
-			}
-		}
+		projectDao.add(project);
 		
-		while(true) {
-			out.println("종료일(예: 2013-08-21) : \n@@?");
-			try {
-				project.setEndDate(Date.valueOf(scanner.nextLine()));
-				break;
-			} catch (Exception e) {
-				out.println("날짜 형식이 맞지 않습니다. 다시 입력하세요!");
-			}
-		}
-		
-		out.println("등록하시겠습니까?(y/n)\n@@?");
-		if(scanner.nextLine().equals("y")){
-			try {
-				projectDao.add(project);
-				out.println("등록되었습니다!");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			out.println("등록 취소하였습니다!");
-		}
+		PrintWriter out = response.getWriter();
+		out.println("등록되었습니다!");
 	}
 
-	private void view(Scanner scanner, PrintStream out, int no) {
+	private void view(ServletRequest request, ServletResponse response)
+		throws Exception
+	{
 		try {
-			this.project = projectDao.get(no);
+			this.project = projectDao.get(
+					Integer.parseInt(request.getParameter("no")));
 			
+			PrintWriter out = response.getWriter();
 			if (project != null) {
 				out.println("번호:" + project.getNo());
 				out.println("제목:" + project.getTitle());
@@ -146,77 +114,58 @@ public class ProjectServlet {
 		}
 	}	
 	
-	private void update(Scanner scanner, PrintStream out) {
+	private void update(ServletRequest request, ServletResponse response)
+		throws Exception
+	{
+		Project project = projectDao.get(
+				Integer.parseInt(request.getParameter("no")));
 		Project copy = project.clone();
-		out.printf("번호(%1$d):\n", project.getNo());
-		out.printf("관리자(%1$s):\n@@?\n", project.getManagerEmail());
-		String value = scanner.nextLine();
-		if (value.length() > 0) {
+		String value = request.getParameter("managerEmail");
+		if (value != null) {
 			copy.setManagerEmail(value);
 		}
-		out.printf("제목(%1$s):\n@@?\n", project.getTitle());
-		value = scanner.nextLine();
-		if (value.length() > 0) {
+		
+		value = request.getParameter("title");
+		if (value != null) {
 			copy.setTitle(value);
 		}
-		out.println("내용(입력종료는 빈라인) : \n@@?");
-		String temp = null;
-		StringBuffer buffer = new StringBuffer();
-		while(true) {
-			temp = scanner.nextLine();
-			if (temp.equals("")) {
-				break;
-			} else {
-				out.println("@@?");
-			}
-			buffer.append(temp + "\n");
+		
+		value = request.getParameter("content");
+		if (value != null) {
+			copy.setContent(value.replace("\\n", "\n"));
 		}
-		copy.setContent(buffer.toString());
 		
-		out.printf("시작일(%1$tY-%1$tm-%1$td) :\n@@?\n", project.getStartDate());
-		try {
-			copy.setStartDate(Date.valueOf(scanner.nextLine()));
-		} catch (Exception e) {}
+		value = request.getParameter("startDate");
+		if (value != null) {
+			copy.setStartDate(Date.valueOf(value));
+		} 
 		
-		out.printf("종료일(%1$tY-%1$tm-%1$td) :\n@@?\n", project.getEndDate());
-		try {
-			copy.setEndDate(Date.valueOf(scanner.nextLine()));
-		} catch (Exception e) {}
+		value = request.getParameter("endDate");
+		if (value != null) {
+			copy.setEndDate(Date.valueOf(value));
+		} 
 		
-		out.println("변경하시겠습니까?(y/n)\n@@?");
-		if(scanner.nextLine().equals("y")){
-			try {
-				int count = projectDao.change(copy);
-				
-				if (count > 0) {
-					out.println("변경되었습니다!");
-				} else {
-					out.println("해당 번호의 프로젝트를 찾을 수 없습니다.");
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		int count = projectDao.change(copy);
+		
+		PrintWriter out = response.getWriter();
+		if (count > 0) {
+			out.println("변경되었습니다!");
 		} else {
-			out.println("변경 취소하였습니다!");
+			out.println("해당 번호의 프로젝트를 찾을 수 없습니다.");
 		}
 	}
-	private void detele(Scanner scanner, PrintStream out) {
-		out.print("정말 삭제하시겠습니까?(y/n)\n@@?\n");
-		if(scanner.nextLine().equals("y")){
-			try {
-				
-				int count = projectDao.remove(project.getNo());
-				
-				if (count > 0) {
-					out.println("삭제되었습니다!");
-				} else {
-					out.println("해당 번호의 프로젝트를 찾을 수 없습니다!");
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+	
+	private void delete(ServletRequest request, ServletResponse response) 
+		throws Exception 
+	{
+		int count = projectDao.remove(
+				Integer.parseInt(request.getParameter("no")));
+		
+		PrintWriter out = response.getWriter();
+		if (count > 0) {
+			out.println("삭제되었습니다!");
 		} else {
-			out.println("삭제 취소하였습니다.");
+			out.println("해당 번호의 프로젝트를 찾을 수 없습니다!");
 		}
 	}
 
