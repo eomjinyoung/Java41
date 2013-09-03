d114d48e0267d14d975efd0f9770c75de993e61e
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-09-03 15:00:39 +0900

    .

--- web04/src/net/bitacademy/java41/servlets/LoginServlet01.java
+++ web04/src/net/bitacademy/java41/servlets/LoginServlet01.java
@@ -8,24 +8,13 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import net.bitacademy.java41.dao.MemberDao;
 import net.bitacademy.java41.vo.Member;
 
-/* 서버에 자동 요청하는 방법
- * 1) Refresh
- * 	- setHeader("Refresh", "1;url=URL");
- * 	- <html><head><meta http-equiv="Refresh" content="1;url=URL"></head>...
- * 
- * 2) Redirect
- *  - sendRedirect("URL");
- */
-@WebServlet("/auth/login")
+//@WebServlet("/auth/login")
 @SuppressWarnings("serial")
-public class LoginServlet extends GenericServlet {
+public class LoginServlet01 extends GenericServlet {
 
 	@Override
 	public void service(ServletRequest request, ServletResponse response)
@@ -41,23 +30,16 @@ public class LoginServlet extends GenericServlet {
 		
 		try {
 			Member member = memberDao.getMember(email, password);
-			HttpSession session = ((HttpServletRequest)request).getSession();
-			
+			out.println("<html><head><title>로그인 결과!</title></head>");
 			if (member != null) {
-				//request.setAttribute("member", member);
-				session.setAttribute("member", member);
-				((HttpServletResponse)response).sendRedirect("../main");
-				
+				out.println("<body><p>로그인 성공입니다!</p>"
+						+ "<p>이메일:" + member.getEmail() + "<br>" 
+						+ "이름:" + member.getName() + "<br>"
+						+ "전화:" + member.getTel() + "</p>"
+						+ "</body></html>");
 			} else {
-				session.invalidate();
-				out.println("<html><head><title>로그인 결과!</title></head>");
 				out.println("<body><p>해당 사용자를 찾을 수 없습니다.</p></body></html>");
-				// setHeader() 메서드는 service() 호출이 끝나기 전까지 호출하면 된다.
-				// 어차피 응답 헤더를 설정하는 명령어기 때문이다.
-				((HttpServletResponse)response).setHeader(
-						"Refresh", "1;url=LoginForm.html");
 			}
-	
 		} catch (Exception e) {
 			e.printStackTrace();
 			out.println("<html><head><title>시스템오류!</title></head>");
--- web04/src/net/bitacademy/java41/servlets/LoginServlet02.java
+++ web04/src/net/bitacademy/java41/servlets/LoginServlet02.java
@@ -8,9 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import net.bitacademy.java41.dao.MemberDao;
 import net.bitacademy.java41.vo.Member;
@@ -21,11 +19,10 @@ import net.bitacademy.java41.vo.Member;
  * 	- <html><head><meta http-equiv="Refresh" content="1;url=URL"></head>...
  * 
  * 2) Redirect
- *  - sendRedirect("URL");
  */
-@WebServlet("/auth/login")
+//@WebServlet("/auth/login")
 @SuppressWarnings("serial")
-public class LoginServlet extends GenericServlet {
+public class LoginServlet02 extends GenericServlet {
 
 	@Override
 	public void service(ServletRequest request, ServletResponse response)
@@ -41,23 +38,40 @@ public class LoginServlet extends GenericServlet {
 		
 		try {
 			Member member = memberDao.getMember(email, password);
-			HttpSession session = ((HttpServletRequest)request).getSession();
-			
+			out.println("<html><head><title>로그인 결과!</title>");
+					
 			if (member != null) {
-				//request.setAttribute("member", member);
-				session.setAttribute("member", member);
-				((HttpServletResponse)response).sendRedirect("../main");
+				out.println(
+					"<meta http-equiv='Refresh' content='1;url=../main'>"
+					+ "</head>");
+				out.println("<body><p>로그인 성공입니다!</p>"
+						+ "<p>이메일:" + member.getEmail() + "<br>" 
+						+ "이름:" + member.getName() + "<br>"
+						+ "전화:" + member.getTel() + "</p>"
+						+ "</body></html>");
 				
+				//((HttpServletResponse)response).setHeader(
+				//		"Refresh", "1;url=../main");
+
 			} else {
-				session.invalidate();
-				out.println("<html><head><title>로그인 결과!</title></head>");
+				out.println(
+					"<meta http-equiv='Refresh' content='1;url=LoginForm.html'>"
+					+ "</head>");
 				out.println("<body><p>해당 사용자를 찾을 수 없습니다.</p></body></html>");
-				// setHeader() 메서드는 service() 호출이 끝나기 전까지 호출하면 된다.
-				// 어차피 응답 헤더를 설정하는 명령어기 때문이다.
-				((HttpServletResponse)response).setHeader(
-						"Refresh", "1;url=LoginForm.html");
+				//((HttpServletResponse)response).setHeader(
+				//		"Refresh", "1;url=LoginForm.html");	
+				/* 응답결과 예시)
+HTTP/1.1 200 OK
+Server: Apache-Coyote/1.1
+Refresh: 1;url=LoginForm.html
+Content-Type: text/html;charset=UTF-8
+Content-Length: 124
+Date: Tue, 03 Sep 2013 04:53:37 GMT
+
+<html><head><title>로그인 결과!</title></head>
+<body><p>해당 사용자를 찾을 수 없습니다.</p></body></html>
+				 */				
 			}
-	
 		} catch (Exception e) {
 			e.printStackTrace();
 			out.println("<html><head><title>시스템오류!</title></head>");
