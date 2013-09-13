245835c276f1d56c3b036ea018b49d29a47e2091
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-09-13 15:19:20 +0900

    .

--- web07/src/net/bitacademy/java41/listeners/ContextLoaderListener.java
+++ web07/src/net/bitacademy/java41/listeners/ContextLoaderListener.java
@@ -1,59 +1,104 @@
 package net.bitacademy.java41.listeners;
 
 import java.io.FileReader;
+import java.lang.reflect.Method;
+import java.util.Collection;
 import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.Properties;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import net.bitacademy.java41.controls.LoginControl;
-import net.bitacademy.java41.controls.LoginFormControl;
-import net.bitacademy.java41.controls.LogoutControl;
-import net.bitacademy.java41.controls.MainControl;
-import net.bitacademy.java41.controls.SigninControl;
-import net.bitacademy.java41.controls.SigninFormControl;
-import net.bitacademy.java41.controls.member.MemberAddControl;
-import net.bitacademy.java41.controls.member.MemberListControl;
-import net.bitacademy.java41.controls.member.MemberViewControl;
-import net.bitacademy.java41.controls.member.PasswordChangeControl;
-import net.bitacademy.java41.controls.project.ProjectAddControl;
-import net.bitacademy.java41.controls.project.ProjectAddFormControl;
-import net.bitacademy.java41.controls.project.ProjectListControl;
-import net.bitacademy.java41.controls.project.ProjectViewControl;
-import net.bitacademy.java41.dao.MemberDao;
-import net.bitacademy.java41.dao.ProjectDao;
-import net.bitacademy.java41.services.AuthService;
-import net.bitacademy.java41.services.MemberService;
-import net.bitacademy.java41.services.ProjectService;
-import net.bitacademy.java41.util.DBConnectionPool;
-
 public class ContextLoaderListener implements ServletContextListener {
+	ServletContext ctx;
+	Hashtable<String,Object> objTable = new Hashtable<String,Object>();
 	
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
-		ServletContext ctx = event.getServletContext();
+		ctx = event.getServletContext();
 		try {
-			loadContextProperties(
+			prepareObjects(
 					ctx.getRealPath("/WEB-INF/context.properties"));
+			prepareDependancy();
+			saveToContext();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private void loadContextProperties(String filePath) throws Exception {
+	private void saveToContext() {
+		Enumeration<String> keyList = objTable.keys();
+		String key = null;
+		while(keyList.hasMoreElements()) {
+			key = keyList.nextElement();
+			ctx.setAttribute(key, objTable.get(key));
+		}
+	}
+
+	private void prepareDependancy() throws Exception {
+		Collection<Object> objList = objTable.values();
+		for(Object obj : objList) {
+			if (obj.getClass() != java.lang.String.class) {
+				injectDependancy(obj);
+			}
+		}
+	}
+
+	@SuppressWarnings("rawtypes")
+	private void injectDependancy(Object obj) throws Exception {
+		System.out.println(obj.getClass().getName() + "---------");
+		
+		Method[] methodList = obj.getClass().getMethods();
+		
+		Class paramClass = null;
+		Object paramObject = null;
+		for (Method m : methodList) {
+			if (m.getName().startsWith("set")) {
+//				System.out.println("====> " + m.getName());
+//				System.out.println("        " + 
+//							m.getParameterTypes()[0].getName());
+				paramClass = m.getParameterTypes()[0];
+				paramObject = findInstanceByClass(paramClass);
+				m.invoke(obj, paramObject);
+			}
+		}
+		
+	}
+
+	@SuppressWarnings("rawtypes")
+	private Object findInstanceByClass(Class paramClass) {
+		Collection<Object> instanceList = objTable.values();
+		for(Object obj : instanceList) {
+			if (obj.getClass() == paramClass) {
+				return obj;
+			}
+		}
+		return null;
+	}
+
+	@SuppressWarnings("rawtypes")
+	private void prepareObjects(String filePath) throws Exception {
 		Properties props = new Properties();
 		props.load( new FileReader(filePath));
 		
 		Enumeration enums = props.keys();
 		String key = null;
 		String value = null;
+		Class clazz = null;
 		while(enums.hasMoreElements()) {
 			key = (String)enums.nextElement();
-			value = (String)props.get(key);
-			System.out.println(key + "=" + value);
+			value = ((String)props.get(key)).trim(); 
+			if (value.charAt(0) == '"') {
+				objTable.put(key, value.substring(1, value.length()-1)); 
+			} else {
+				clazz = Class.forName(value);
+				objTable.put(key, clazz.newInstance());
+			} 
 		}
+		
 	}
 
 	@Override
