0ad3198d82568baf06db1df4e15370b37ada91e2
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-09-25 15:21:49 +0900

    .

--- web09/src/net/bitacademy/java41/test/t05/Spring05.java
+++ web09/src/net/bitacademy/java41/test/t05/Spring05.java
@@ -9,6 +9,18 @@ public class Spring05 {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext(
 			"net/bitacademy/java41/test/t05/application-context.xml");
 		
+		Student3 student = (Student3)ctx.getBean("st3");
+		School school = student.school;
+		System.out.println(school.getName());
+		
+		Award award = student.award;
+		System.out.println(award.title);
+	}
+	
+	public static void main02(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+			"net/bitacademy/java41/test/t05/application-context.xml");
+		
 		Student2 student = (Student2)ctx.getBean("st2");
 		School school = student.school;
 		System.out.println(school.getName());
