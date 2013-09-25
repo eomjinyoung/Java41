ee82e001246d0487ab18433193b48160fb4d2aac
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-09-25 14:50:00 +0900

    .

--- web09/src/net/bitacademy/java41/test/t05/Spring05.java
+++ web09/src/net/bitacademy/java41/test/t05/Spring05.java
@@ -9,6 +9,15 @@ public class Spring05 {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext(
 			"net/bitacademy/java41/test/t05/application-context.xml");
 		
+		Student2 student = (Student2)ctx.getBean("st2");
+		School school = student.school;
+		System.out.println(school.getName());
+	}
+	
+	public static void main01(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+			"net/bitacademy/java41/test/t05/application-context.xml");
+		
 		Student student = (Student)ctx.getBean("st1");
 		School school = student.getSchool();
 		Award award = student.getAward();
--- web09/src/net/bitacademy/java41/test/t05/Student2.java
+++ web09/src/net/bitacademy/java41/test/t05/Student2.java
@@ -3,16 +3,19 @@ package net.bitacademy.java41.test.t05;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 
-public class Student {
-	private String name;
-	private int age;
-	private String tel;
-	private School school;
-	private Award award;
+public class Student2 {
+	protected String name;
+	protected int age;
+	protected String tel;
 	
-	public Student() {}
+	@Autowired
+	protected School school;
 	
-	public Student(String name) {
+	protected Award award;
+	
+	public Student2() {}
+	
+	public Student2(String name) {
 		this.name = name;
 	}
 	
@@ -37,20 +40,6 @@ public class Student {
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
-	public School getSchool() {
-		return school;
-	}
-	
-	// school 프로퍼티에 대해서만 자동으로 의존 객체를 주입한다.(byType)
-	/* @Autowired
-	 * - 기본은 해당 타입의 의존객체가 없다면 오류가 발생한다.
-	 * - required를 false로 설정하면, 선택 사항이 된다.
-	 */
-	@Autowired(required=false)
-	public void setSchool(School school) {
-		this.school = school;
-	}
-	
 	public Award getAward() {
 		return award;
 	}
