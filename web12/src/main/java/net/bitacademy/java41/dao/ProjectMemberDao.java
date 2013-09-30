439ff0bff406b17dfce889ef6b358595e374879d
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-09-30 13:53:32 +0900

    .

---
+++ web12/src/main/java/net/bitacademy/java41/dao/ProjectMemberDao.java
@@ -0,0 +1,10 @@
+package net.bitacademy.java41.dao;
+
+import java.util.Map;
+
+public interface ProjectMemberDao {
+	
+	int add(Map<String,Object> paramMap) throws Exception;
+	
+	int deleteAll(int no) throws Exception;
+}
