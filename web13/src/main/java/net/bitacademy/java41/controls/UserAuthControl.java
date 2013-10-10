52d55be661b321af314a671d1a6c20b20cfccb92
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-10-10 15:16:48 +0900

    .

--- web13/src/main/java/net/bitacademy/java41/controls/UserAuthControl.java
+++ web13/src/main/java/net/bitacademy/java41/controls/UserAuthControl.java
@@ -5,17 +5,24 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import net.bitacademy.java41.services.AuthService;
+import net.bitacademy.java41.vo.JsonResult;
 import net.bitacademy.java41.vo.LoginInfo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
+import com.google.gson.Gson;
 
 @Controller
 @SessionAttributes("loginInfo")
@@ -47,7 +54,84 @@ public class UserAuthControl {
 	
 	@RequestMapping(value="/login",
 			method=RequestMethod.POST)
-	public String login(
+	public ResponseEntity<String> login(
+			String email,
+			@RequestParam("password") String pwd,
+			String saveId,
+			HttpServletResponse response,
+			HttpSession session,
+			Model model,
+			SessionStatus status) throws Exception {
+		LoginInfo loginInfo = authService.getLoginInfo(email, pwd);
+		
+		if(saveId != null) {
+			Cookie cookie = new Cookie("email", email);
+			cookie.setMaxAge(60); 
+			response.addCookie(cookie);
+		} else {
+			Cookie cookie = new Cookie("email", null);
+			cookie.setMaxAge(0); 
+			response.addCookie(cookie);
+		}
+		
+		JsonResult jsonResult = null;
+		if (loginInfo != null) {
+			model.addAttribute("loginInfo", loginInfo);
+			jsonResult = new JsonResult().setStatus("success")
+								.setData("한글");
+		} else {
+			status.setComplete();
+			jsonResult = new JsonResult().setStatus("fail");
+		}
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Type", "text/plain;charset=UTF-8");
+		return new ResponseEntity<String>(
+						new Gson().toJson( jsonResult ),
+						headers,
+						HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value="/login2",
+			method=RequestMethod.POST)
+	@ResponseBody /* 리턴 값이 응답 데이터임을 선언 */
+	public String login2(
+			String email,
+			@RequestParam("password") String pwd,
+			String saveId,
+			HttpServletResponse response,
+			HttpSession session,
+			Model model,
+			SessionStatus status) throws Exception {
+		LoginInfo loginInfo = authService.getLoginInfo(email, pwd);
+		
+		if(saveId != null) {
+			Cookie cookie = new Cookie("email", email);
+			cookie.setMaxAge(60); 
+			response.addCookie(cookie);
+		} else {
+			Cookie cookie = new Cookie("email", null);
+			cookie.setMaxAge(0); 
+			response.addCookie(cookie);
+		}
+		
+		JsonResult jsonResult = null;
+		if (loginInfo != null) {
+			model.addAttribute("loginInfo", loginInfo);
+			jsonResult = new JsonResult().setStatus("success")
+								.setData("한글");
+		} else {
+			status.setComplete();
+			jsonResult = new JsonResult().setStatus("fail");
+		}
+		
+		response.setContentType("text/plain;charset=UTF-8");
+		return new Gson().toJson( jsonResult );
+	}
+	
+	@RequestMapping(value="/login1",
+			method=RequestMethod.POST)
+	public String login1(
 			String email,
 			@RequestParam("password") String pwd,
 			String saveId,
@@ -69,12 +153,17 @@ public class UserAuthControl {
 		
 		if (loginInfo != null) {
 			model.addAttribute("loginInfo", loginInfo);
-			return "redirect:../main.do";
-			
+			model.addAttribute("jsonResult", new JsonResult()
+												.setStatus("success") );
+			//model.addAttribute("status", "success");
 		} else {
 			status.setComplete();
-			return "auth/loginFail";
+			model.addAttribute("jsonResult", new JsonResult()
+												.setStatus("fail") );
+			//model.addAttribute("status", "fail");
 		}
+		
+		return "auth/loginResult";
 	}
 
 }
--- web13/src/main/webapp/auth/login.js
+++ web13/src/main/webapp/auth/login.js
@@ -18,7 +18,24 @@ window.onload = function() {
 		xhr.open("POST", "login.do", false); // 2. 요청정보준비(동기)
 		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); // only post
 		xhr.send(params); // 3. 요청(서버에서 응답이 올 때까지 리턴안함)
-		console.log(xhr.responseText); // 4. 서버의 응답 꺼내기
+		//console.log(xhr.responseText); // 4. 서버의 응답 꺼내기
+		
+		// 자바스크립트 객체 생성 코드를 실행하는 방법
+		//1) eval()
+		//var result = eval("[" + xhr.responseText + "]")[0];
+		//2) JSON객체 사용
+		// - JSON에서 속성 이름은 반드시 ""을 사용해야 한다.
+		// - 문자열도 반드시 ""을 사용해야 한다.
+		var result = JSON.parse(xhr.responseText);
+		
+		
+		if(result.status == "fail") {
+			alert("이메일이나 암호가 맞지 않습니다.");
+		} else {
+			alert("로그인 성공입니다.");
+			alert(result.data);
+		}
+		
 		
 		return false;
 	};
